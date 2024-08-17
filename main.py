8bad5fa025ac990d210e1c716ac62eab0cbb43af
IshaqKHATTAK <mi5315562@gmail.com>
2024-08-17 14:30:34 +0500

    post method and request body

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 @app.get('/')
 async def index():
@@ -15,3 +14,26 @@ async def about():
 @app.get('abouot/id')
 async def about_id(id):
     return {'response':id}
+
+#query parameters
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get('/items/')
+async def items(skip: int = 0, limit: int = 10):
+    return {'response':fake_items_db[skip: skip+limit]}
+
+#request body and post method
+
+@app.post('/post')
+async def data():
+    return {'response':'data that is send'}
+
+from pydantic import BaseModel
+class Intro(BaseModel):
+    name: str
+    age: int | None = None
+    description: str | None = None
+
+@app.post('/into')
+async def intro(info: Intro):
+    return {'response':info}
