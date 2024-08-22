756aa59e71e36fb5887a306cd2c5c6d1d5ddfcec
IshaqKHATTAK <mi5315562@gmail.com>
2024-08-22 06:28:02 +0500

    adding chnages and the database files

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI,HTTPException,Depends
+from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
 app = FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 @app.get('/')
 async def index():
     return {'response':'this is index route'}
+
+@app.get("/items/")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
 
 @app.get('/about')
 async def about():
@@ -28,12 +34,27 @@ async def items(skip: int = 0, limit: int = 10):
 async def data():
     return {'response':'data that is send'}
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 class Intro(BaseModel):
-    name: str
+    name: str = Field(examples=["namanam"])
     age: int | None = None
-    description: str | None = None
+    description: str | None = Field(
+        default=None, title="The description of the item", max_length=300
+    )
 
 @app.post('/into')
 async def intro(info: Intro):
     return {'response':info}
+
+items = {"foo": "The Foo Wrestlers"}
+
+
+@app.get("/items-header/{item_id}")
+async def read_item_header(item_id: str):
+    if item_id not in items:
+        raise HTTPException(
+            status_code=404,
+            detail="Item not found",
+            headers={"X-Error": "There goes my error"},
+        )
+    return {"item": items[item_id]}
